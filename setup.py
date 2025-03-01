b4e3df729f45d291ee3a32851bf4476a1c027193
Henry Tian <chariothy@gmail.com>
2025-03-01 23:10:13 +0800

    Remove dependencies of pytest.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ PACKAGES = ['pybeans']
 URL = 'https://github.com/chariothy/pybeans.git'
 EMAIL = 'chariothy@gmail.com'
 AUTHOR = 'Henry TIAN'
-VERSION = '1.12.3'
+VERSION = '1.12.4'
 
 LONG_DESCRIPTION = '''
 This is a helper which includes common methods and classes.
@@ -22,8 +22,7 @@ This is a helper which includes common methods and classes.
 # What packages are required for this module to be executed?
 REQUIRED = [
     "colorama>=0.4.4",
-    "requests>=2.32.0",
-    "pytest>=7.4.3"
+    "requests>=2.32.0"
 ]
 
 # The rest you shouldn't have to touch too much :)
